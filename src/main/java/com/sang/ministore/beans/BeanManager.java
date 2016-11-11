e1964964e5b4d109f87b49bd9720d6bcc390e378
Y2016j2e <Imic2016>
2016-11-11 10:33:54 +0700

    show List data on tabview , tab

--- src/main/java/com/sang/ministore/beans/BeanManager.java
+++ src/main/java/com/sang/ministore/beans/BeanManager.java
@@ -2,23 +2,77 @@ package com.sang.ministore.beans;
 
 import org.primefaces.event.ResizeEvent;
 
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Named;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Created by Sang on 11/10/2016.
- */
-@Named(value = "beanManager")
-@RequestScoped
+
+@ManagedBean(name = "beanManager")
+@SessionScoped
 public class BeanManager {
 
+    private String selectionType;
 
+    private List<Legend>  weapon = new ArrayList();
+
+    public List<Legend> getWeapon() {
+        Legend item1 = new Legend();
+        item1.setType("One Hand");
+        item1.setPrice(100);
+        item1.setDesciption("Make by Tom cuirse");
+
+        Legend item2 = new Legend();
+        item2.setType("Two Hand");
+        item2.setPrice(150);
+        item2.setDesciption("Make by Hilray");
+        Legend item3 = new Legend();
+        item3.setType("Knife");
+        item3.setPrice(80);
+        item3.setDesciption("Make by Jason");
+        Legend item4 = new Legend();
+        item4.setType("Staff");
+        item4.setPrice(100);
+        item4.setDesciption("Make by Aladin");
+
+        weapon = new ArrayList();
+        weapon.add(item1);
+        weapon.add(item2);
+        weapon.add(item3);
+        weapon.add(item4);
+        return weapon;
+    }
+
+    public void setWeapon(List<Legend> weapon) {
+        this.weapon = weapon;
+    }
 
     public BeanManager() {
     }
+    public String legendary(){
+        selectionType ="legendary";
+        return "automobile";
+    }
+
+    public String rare(){
+        selectionType ="rare";
+        return "automobile";
+    }
+
+    public String common(){
+        selectionType = "common";
+        return "automobile";
+    }
+
 
     public void layoutResizeEvent(ResizeEvent event){
-    System.out.println("Layout has been sized" + event.getComponent());
-}
+    System.out.println("Layout has been sized" + event.getComponent()); }
 
+    public String getSelectionType() {
+        return selectionType;
+    }
+
+    public void setSelectionType(String selectionType) {
+        this.selectionType = selectionType;
+    }
 }
